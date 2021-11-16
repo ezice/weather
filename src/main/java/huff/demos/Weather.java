58030b8a3f48ae7a77c01092875223563cb619a7
Rick Huff <rhuff2@yahoo.com>
2021-11-16 11:43:33 -0700

    Initial commit.

---
+++ src/main/java/huff/demos/Weather.java
@@ -0,0 +1,13 @@
+package huff.demos;
+
+/**
+ * Hello world!
+ *
+ */
+public class Weather 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
